5fcd155a162d81ec1e8c9da22c42191abc3c108a
bubanoid <an.andrusyk@gmail.com>
2015-12-24 15:25:20 +0200

    m

--- profapp/utils/redirect_url.py
+++ profapp/utils/redirect_url.py
@@ -31,22 +31,22 @@ def url_page(endpoint=None, **kwargs):
     ep = endpoint if endpoint else request.endpoint
     kwargs_new = request.view_args
 
-#    try:
-    from run import app
-    with app.app_context():
-        func = current_app.view_functions[ep]
-#    except:
-#        from run_front import app_front
-#        with app_front.app_context():
-#            func = current_app.view_functions[ep]
-
-#    print(func)
-    argspec = inspect.getargspec(func)
-    args = argspec.args
-    defaults = argspec.defaults or []
-
-    len_obligatory_args = len(args) - len(defaults)
-    obligatory_args = args[0:len_obligatory_args]
+# #    try:
+#     from run import app
+#     with app.app_context():
+#         func = current_app.view_functions[ep]
+# #    except:
+# #        from run_front import app_front
+# #        with app_front.app_context():
+# #            func = current_app.view_functions[ep]
+#
+# #    print(func)
+#     argspec = inspect.getargspec(func)
+#     args = argspec.args
+#     defaults = argspec.defaults or []
+#
+#     len_obligatory_args = len(args) - len(defaults)
+#     obligatory_args = args[0:len_obligatory_args]
 
     if not set(obligatory_args).issubset(set(kwargs.keys())):
         raise WrongMandatoryParametersPassedToFunction
